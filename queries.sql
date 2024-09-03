d57cb35aab5bd8810252f8e316ef4eb3180cf72c
tvaltn <115498434+tvaltn@users.noreply.github.com>
2024-09-03 11:58:50 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -80,4 +80,52 @@ SELECT COUNT(id) FROM films
 --Return the average film rating by genre
 SELECT genre, AVG(score) FROM films GROUP BY genre
 
+-- Create directors table
+CREATE TABLE directors(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+);
 
+-- Add a few directors...
+INSERT INTO directors(name)
+VALUES
+	('John Cena'),
+	('Ola Nordmann'),
+	('Nigel Nigel'),
+	('AJ AJ')
+
+-- Recreate the films table...
+DROP TABLE films
+CREATE TABLE films(
+	id SERIAL PRIMARY KEY,
+	title TEXT UNIQUE NOT NULL,
+	genre TEXT NOT NULL,
+	release_year INT NOT NULL,
+	score INT NOT NULL,
+	directorId INT NOT NULL
+);
+
+-- Re-insert the film data...
+INSERT INTO films(title, genre, release_year, score, directorId)
+VALUES
+	('The Shawshank Redemption', 'Drama', 1994, 9, 3),
+	('The Godfather', 'Crime', 1972, 9, 4),
+	('The Dark Knight', 'Action', 2008, 9, 3),
+	('Alien', 'SciFi', 1979, 9, 2),
+	('Total Recall', 'SciFi', 1990, 8, 1),
+	('The Matrix', 'SciFi', 1999, 8, 1),
+	('The Matrix Resurrections', 'SciFi', 2021, 5, 4),
+	('The Matrix Reloaded', 'SciFi', 2003, 6, 3),
+	('The Hunt for Red October', 'Thriller', 1990, 7, 2),
+	('Misery', 'Thriller', 1990, 7, 1),
+	('The Power Of The Dog', 'Western', 2021, 6, 4),
+	('Hell or High Water', 'Western', 2016, 8, 2),
+	('The Good the Bad and the Ugly', 'Western', 1966, 9, 1),
+	('Unforgiven', 'Western', 1992, 7, 2)
+
+-- Return list of films with their director using JOIN...
+SELECT f.title, d.name AS director FROM films f LEFT JOIN directors d ON f.directorid = d.id
+
+
+-- Return list of directors alongside the number of films they have directed
+SELECT d.name AS director, COUNT(f.directorId) FROM films f LEFT JOIN directors d ON f.directorId = d.id GROUP BY d.name
